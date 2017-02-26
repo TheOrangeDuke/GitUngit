2755c6964074e96fbd03403e9b3ebdb08528907d
Eduard Paraschivescu <ed.paraschivescu@gmail.com>
2017-02-25 19:37:23 -0500

    Changed behaviour

--- test.js
+++ test.js
@@ -1,15 +1,4 @@
-function bob(){
-    console.log("Hi, my name is bob");
-}
-
-function robert(){
-    console.log("Hi, my name is robert");
-}
-
 function name(){
-    // calls function bob
-    bob();
-
-    // calls function robert
-    robert();
+    console.log("Hi, my name is robert");
+    console.log("Hi, my name is bob");
 }
