d42334f919976faa674eec7ba849983be38deac7
Eduard Paraschivescu <ed.paraschivescu@gmail.com>
2017-02-25 19:49:49 -0500

    Added clock function

--- test.js
+++ test.js
@@ -4,4 +4,15 @@
 function name(){
     console.log("Hi, my name is robert");
     console.log("Hi, my name is bob");
+
+    clock();
+}
+
+function clock(){
+    var now = new Date();
+    var h = now.getHours();
+    var m = now.getMinutes();
+    var s = now.getSeconds();
+
+    console.log(h + ":" + m + ":" + s);
 }
