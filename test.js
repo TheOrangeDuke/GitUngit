567c08721fe7c30d0b10f56459f78488cfef4354
Eduard Paraschivescu <ed.paraschivescu@gmail.com>
2017-02-25 19:42:23 -0500

    Added rand function

--- test.js
+++ test.js
@@ -4,4 +4,11 @@
 function name(){
     console.log("Hi, my name is robert");
     console.log("Hi, my name is bob");
+
+    randomNumber();
+}
+
+function randomNumber(){
+    var alpha = Math.floor(Math.random() * 11);
+    console.log(alpha);
 }
